f1570cfdfbf94eed64b575df3eaa03ca798ca6c8
fanny <fannyluzcc@gmail.com>
2017-08-26 11:42:22 -0500

    Se adiciona capa de distribucion

---
+++ Enuesta.Distribucion.API/Controllers/PruebasController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Enuesta.Distribucion.API.Controllers
+{
+    public class PruebasController : ApiController
+    {
+        // GET: api/Pruebas
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Pruebas/5
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+        // POST: api/Pruebas
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Pruebas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Pruebas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Enuesta.Distribucion.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Enuesta.Distribucion.API.WebApiApplication" Language="C#" %>
