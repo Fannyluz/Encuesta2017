5f77eb279c1fd0bda7210afe83c55f232de18379
fanny <fannyluzcc@gmail.com>
2017-08-26 12:19:08 -0500

    nuevo controlador

---
+++ Enuesta.Distribucion.API/Controllers/CruzController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Enuesta.Distribucion.API.Controllers
+{
+    public class CruzController : ApiController
+    {
+        // GET: api/Cruz
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Cruz/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Cruz
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Cruz/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Cruz/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
